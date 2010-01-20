6c7ced9aeb80f789259e4fe09ee37402f3478bde
pdw <>
2010-01-20 22:32:50 +0000

    Fix for armeb bug.

--- ether.h
+++ ether.h
@@ -12,7 +12,8 @@ struct	ether_header {
 	u_int8_t	ether_dhost[ETHER_ADDR_LEN];
 	u_int8_t	ether_shost[ETHER_ADDR_LEN];
 	u_int16_t	ether_type;
-};
+} __attribute__((packed));
+
 
 struct vlan_8021q_header {
 	u_int16_t	priority_cfi_vid;
