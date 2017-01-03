5340a3b9af25e3330ca7e234aff159cda19e9ec7
Paul Warren <pdw@ex-parrot.com>
2017-01-03 21:50:49 +0000

    Missing header file.

---
+++ tui.h
@@ -0,0 +1,15 @@
+/*
+ * tui.h:
+ *
+ *
+ */
+
+#ifndef __TUI_H_ /* include guard */
+#define __TUI_H_
+
+void tui_print(void);
+void tui_init(void);
+void tui_loop(void);
+void tui_tick(int);
+
+#endif /* __TUI_H_ */
